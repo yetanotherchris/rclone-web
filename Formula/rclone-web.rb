031fbfd5c09da0f9a7faa750d6ffcff18007d13d
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2026-07-17 00:27:35 +0000

    chore: update package manifests to v1.2.0

--- Formula/rclone-web.rb
+++ Formula/rclone-web.rb
@@ -1,25 +1,25 @@
 class RcloneWeb < Formula
   desc "Web frontend for rclone to manage and run jobs from your browser"
   homepage "https://github.com/yetanotherchris/rclone-web"
-  version "1.1.0"
+  version "1.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/rclone-web/releases/download/v1.1.0/rclone-web-darwin-arm64.tar.gz"
-      sha256 "2acfae5d3e95f6376516fd5bcff4988bab1cc58be2e0e41b6d5f346d1c1399b6"
+      url "https://github.com/yetanotherchris/rclone-web/releases/download/v1.2.0/rclone-web-darwin-arm64.tar.gz"
+      sha256 "8930972471c522b08ad9d013d53c0717a46570b880d46f670286ed611b0cfc0c"
     else
-      url "https://github.com/yetanotherchris/rclone-web/releases/download/v1.1.0/rclone-web-darwin-amd64.tar.gz"
-      sha256 "6e30e7c47396aec558f7bc77b3cba318f2657043231f0bfe67d0b5a7ce45fc5e"
+      url "https://github.com/yetanotherchris/rclone-web/releases/download/v1.2.0/rclone-web-darwin-amd64.tar.gz"
+      sha256 "edc878176f550efea4d67ad59b2b8b7dc84467dd9f8877c536044051a55b37cc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/rclone-web/releases/download/v1.1.0/rclone-web-linux-arm64.tar.gz"
-      sha256 "0f25057017c968dcf7b5a6f592046a7f32d558286bab262f89a2b5e617ca0e22"
+      url "https://github.com/yetanotherchris/rclone-web/releases/download/v1.2.0/rclone-web-linux-arm64.tar.gz"
+      sha256 "e05b3ab5e782e76caca8b8e30d8fc432452fc9989938da1d44260343b56cce4a"
     else
-      url "https://github.com/yetanotherchris/rclone-web/releases/download/v1.1.0/rclone-web-linux-amd64.tar.gz"
-      sha256 "046756ef0d7dbd6e49ac4ad87d56f0fa59d239e1bce0b5b2dea28276890e4ef1"
+      url "https://github.com/yetanotherchris/rclone-web/releases/download/v1.2.0/rclone-web-linux-amd64.tar.gz"
+      sha256 "bdc0856b37e92af7fbb1d49678f33be7b179e60ebab3de9f1b3707f2f3e4aa9f"
     end
   end
 
